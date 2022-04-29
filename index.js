e50a277b62b9df892caef9f9e9c308d2327239d9
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-29 11:47:20 +0600

    env file done

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
-
 app.use(cors());
 app.use(express.json());
+
+//dbuser5
+//sE9MSxpOlIxMPp4z
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfb5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("connected");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Server Is Running");
